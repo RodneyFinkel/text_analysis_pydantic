d6e7d6628036226dca813e80af05ae54b879b6c0
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2026-03-06 18:22:26 +0200

    map-reduce

--- processor.py
+++ processor.py
@@ -8,7 +8,7 @@ from langchain_community.utilities import SQLDatabase
 from langchain_core.output_parsers import StrOutputParser # change to StrOutputParser for raw string output in DB queries
 from dotenv import load_dotenv
 import sqlparse
-
+# python code generator with ---st.code(result["sql"], language="sql") --- in app.py to format the SQL query output nicely in Streamlit and use for next method to build 
 load_dotenv()
 
 # Pydantic Schemas for Structured Output
